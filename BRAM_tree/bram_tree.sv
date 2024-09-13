38d18380e32264d5e711c4758a6a852f3929fc89
Charlie Wu <charliewu0902@hotmail.com>
2024-09-13 11:26:52 -0400

    BRAM tree midway commit, even levels are done, but not odd levels.

--- BRAM_tree/bram_tree.sv
+++ BRAM_tree/bram_tree.sv
@@ -31,7 +31,7 @@ module bram_tree (
       xilinx_true_dual_port_read_first_1_clock_ram #(
           .RAM_WIDTH(32),
           .RAM_DEPTH(2 ** i),
-          .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
+          .RAM_PERFORMANCE("LOW_LATENCY"),
           .INIT_FILE("")
       ) bram (
           .addra(bram_addr_a[i]),  // Port A address bus, width determined from RAM_DEPTH
@@ -45,8 +45,8 @@ module bram_tree (
           .enb(1'b1),       // Port B RAM Enable, for additional power savings, disable port when not in use
           .rsta(rst),  // Port A output reset (does not affect memory contents)
           .rstb(rst),  // Port B output reset (does not affect memory contents)
-          .regcea(1'b1),  // Port A output register enable
-          .regceb(1'b1),  // Port B output register enable
+          .regcea(1'b0),  // Port A output register enable
+          .regceb(1'b0),  // Port B output register enable
           .douta(bram_dout_a[i]),  // Port A RAM output data, width determined from RAM_WIDTH
           .doutb(bram_dout_b[i])  // Port B RAM output data, width determined from RAM_WIDTH
       );
@@ -68,15 +68,18 @@ module bram_tree (
     end
   endgenerate
 
-  enum logic [1:0] {
-    REPLACE = 2'b00,
-    EVEN = 2'b01,
-    ODD = 2'b10,
-    IDLE = 2'b11
+  enum logic [2:0] {
+    IDLE = 3'b000,
+    REPLACE = 3'b001,
+    EVEN = 3'b010,
+    ODD = 3'b011,
+    READ = 3'b100,
+    WRITE = 3'b101
   } STATE;
 
-  // logic even_odd_flag;
-  integer level, addr, done_flag;
+  integer level, addr;
+
+  // Synchronized Finite State Machine
   always_ff @(posedge clk) begin
     if (rst) begin
       for (int j = 0; j < 4; j++) begin
@@ -96,9 +99,7 @@ module bram_tree (
         new_right_child[k] <= '0;
       end
       level <= 0;
-      addr <= 0;
-      // even_odd_flag <= 0;
-      done_flag <= 0;
+      addr  <= 0;
       STATE <= IDLE;
     end else begin
       case (STATE)
@@ -111,105 +112,51 @@ module bram_tree (
         end
 
         REPLACE: begin
-          bram_addr_b[0] <= '0;
-          bram_din_b[0] <= new_item;
-          bram_we_b[0] <= '1;
-          STATE <= EVEN;
+          STATE <= READ;
         end
 
         EVEN: begin
-          // close all wea ports just in case 
-          for (int k = 0; k < 4; k++) begin
-            bram_we_a[k] <= '0;
-            bram_we_b[k] <= '0;
-          end
-
-          // for (level = 0; level < 4; level = level + 2) begin
-          // for (addr = 0; addr < (1 << level); addr++) begin
-          // if (level < 4) begin
-          // if (level != 3) begin  // not equal to second last level, for scaling purpose in future
-          // if (addr < (1 << level)) begin
-          // bram_addr_b[level]   <= addr;
-          // bram_addr_a[level+1] <= (addr << 1);
-          // bram_addr_b[level+1] <= (addr << 1) + 1;
-          // read from BRAMs
-          // parent[(1<<level)-1+addr]      <= bram_dout_b[level];
-          // left_child[(1<<level)-1+addr]  <= bram_dout_a[level+1];
-          // right_child[(1<<level)-1+addr] <= bram_dout_b[level+1];
-          // get results from comparators
-          // bram_din_b[level]              <= new_parent[(1<<level)-1+addr];
-          // bram_din_a[level+1]            <= new_left_child[(1<<level)-1+addr];
-          // bram_din_b[level+1]            <= new_right_child[(1<<level)-1+addr];
-          // write into BRAMS
-          // bram_we_b[level]               <= '1;
-          // bram_we_a[level+1]             <= '1;
-          // bram_we_b[level+1]             <= '1;
-          // end
-          // end
-          // end
-          // end
-
           if (replace) begin
             STATE <= REPLACE;
-          end else if (level == 2 && addr == 3) begin  //TODO: need to be scalable.
-            level <= 0;
-            addr <= 0;
-            done_flag <= 0;
-            // even_odd_flag <= 0;
-            STATE <= ODD;
           end else begin
-            if (addr == (1 << level) - 1) begin
-              level <= level + 2;
-              addr  <= 0;
-            end else begin
-              addr <= addr + 1;
-            end
+            STATE <= READ;
           end
         end
 
         ODD: begin
-          // close all wea ports
-          for (int k = 0; k < 4; k++) begin
-            bram_we_a[k] <= '0;
-            bram_we_b[k] <= '0;
-          end
-          // for (level = 1; level < 4; level = level + 2) begin
-          if (level < 4) begin
-            if (level != 3) begin  // not equal to second last level
-              if (addr < (1 << level)) begin
-                if (addr == 1) begin
-                  done_flag <= 1;
-                end
-                bram_addr_b[level]   <= addr;
-                bram_addr_a[level+1] <= (addr << 1);
-                bram_addr_b[level+1] <= (addr << 1) + 1;
-                // read from BRAMs
-                // parent[(1<<level)-1+addr]      <= bram_dout_b[level];
-                // left_child[(1<<level)-1+addr]  <= bram_dout_a[level+1];
-                // right_child[(1<<level)-1+addr] <= bram_dout_b[level+1];
-                // get results from comparators
-                // bram_din_b[level]              <= new_parent[(1<<level)-1+addr];
-                // bram_din_a[level+1]            <= new_left_child[(1<<level)-1+addr];
-                // bram_din_b[level+1]            <= new_right_child[(1<<level)-1+addr];
-                // write into BRAMS
-                // bram_we_b[level]               <= '1;
-                // bram_we_a[level+1]             <= '1;
-                // bram_we_b[level+1]             <= '1;
-              end
-            end
-          end
           if (replace) begin
             STATE <= REPLACE;
-          end else if (done_flag == 1) begin
-            level <= 0;
-            addr <= 0;
-            done_flag <= 0;
-            // even_odd_flag <= 0;
-            STATE <= EVEN;
           end else begin
-            level <= level + 2;
-            addr  <= addr + 1;
+            STATE <= READ;
           end
+        end
+
+        READ: begin
+          if (addr == (1 << level) - 1) begin
+            level <= level + 2;
+            addr  <= 0;
+          end else begin
+            addr <= addr + 1;
+          end
+          if (level == 2 && addr == 3) begin  //TODO: need to be scalable.
+            level <= 1;
+            addr  <= 0;
+            STATE <= ODD;
+          end
+          STATE <= EVEN;
+        end
+
+        WRITE: begin
+          if (replace) begin
+            STATE <= REPLACE;
+          end else if (level == 0) begin
+            STATE <= EVEN;
+          end else if (level == 1) begin
+            STATE <= ODD;
+          end
+        end
+
+        default: begin
         end
       endcase
     end
@@ -219,34 +166,72 @@ module bram_tree (
     case (STATE)
       IDLE: begin
       end
+
       REPLACE: begin
+        bram_addr_b[0] = '0;
+        bram_din_b[0]  = new_item;
+        bram_we_b[0]   = '1;
       end
+
       EVEN: begin
+        // close all wea ports just in case 
+        for (int k = 0; k < 4; k++) begin
+          bram_we_a[k] = '0;
+          bram_we_b[k] = '0;
+        end
+        // Giving address to BRAMs
+        if (level < 4) begin
+          if (level != 3) begin  //* not equal to second last level, for scaling purpose in future
+            if (addr < (1 << level)) begin
+              bram_addr_b[level]   = addr;
+              bram_addr_a[level+1] = (addr << 1);
+              bram_addr_b[level+1] = (addr << 1) + 1;
+            end
+          end
+        end
+      end
+
+      ODD: begin
+        // close all wea ports just in case 
+        for (int k = 0; k < 4; k++) begin
+          bram_we_a[k] = '0;
+          bram_we_b[k] = '0;
+        end
+        // Giving address to BRAMs
         if (level < 4) begin
           if (level != 3) begin  // not equal to second last level, for scaling purpose in future
             if (addr < (1 << level)) begin
               bram_addr_b[level]   = addr;
               bram_addr_a[level+1] = (addr << 1);
               bram_addr_b[level+1] = (addr << 1) + 1;
-              if (bram_addr_a[level+1] != bram_addr_b[level+1]) begin
-                // read from BRAMs
-                parent[(1<<level)-1+addr]      = bram_dout_b[level];
-                left_child[(1<<level)-1+addr]  = bram_dout_a[level+1];
-                right_child[(1<<level)-1+addr] = bram_dout_b[level+1];
-                // get results from comparators
-                bram_din_b[level]              = new_parent[(1<<level)-1+addr];
-                bram_din_a[level+1]            = new_left_child[(1<<level)-1+addr];
-                bram_din_b[level+1]            = new_right_child[(1<<level)-1+addr];
-                // write into BRAMS
-                // bram_we_b[level]               <= '1;
-                // bram_we_a[level+1]             <= '1;
-                // bram_we_b[level+1]             <= '1;
-              end
             end
           end
         end
       end
-      ODD: begin
+
+      READ: begin
+        // read from BRAMs
+        parent[(1<<level)-1+addr]      = bram_dout_b[level];
+        left_child[(1<<level)-1+addr]  = bram_dout_a[level+1];
+        right_child[(1<<level)-1+addr] = bram_dout_b[level+1];
+        // get results from comparators
+        bram_din_b[level]              = new_parent[(1<<level)-1+addr];
+        bram_din_a[level+1]            = new_left_child[(1<<level)-1+addr];
+        bram_din_b[level+1]            = new_right_child[(1<<level)-1+addr];
+      end
+
+      WRITE: begin
+        // read from BRAMs
+        parent[(1<<level)-1+addr]      = bram_dout_b[level];
+        left_child[(1<<level)-1+addr]  = bram_dout_a[level+1];
+        right_child[(1<<level)-1+addr] = bram_dout_b[level+1];
+        // write into BRAMS
+        bram_we_b[level]               = '1;
+        bram_we_a[level+1]             = '1;
+        bram_we_b[level+1]             = '1;
+      end
+
+      default: begin
       end
     endcase
   end
